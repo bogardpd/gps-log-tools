984b5ac3d53300a49fd0c7b2ece349b461899840
Paul Bogard <paulbogard@gmail.com>
2022-02-21 11:21:26 -0500

    Create Get-MTP-Object function in Import from Garmin

--- Import from Garmin.ps1
+++ Import from Garmin.ps1
@@ -1,38 +1,38 @@
-# Automates importing a GPX file from a Garmin GPS. Backs up Driving.kml,
-# copies the GPX track log from the Garmin to a GPX archive folder (named with
-# a UTC timestamp) and imports the GPX tracks into Driving.kml using a Python
-# script.
+# Automates importing a GPX file from a Garmin GPS. Copies the GPX track log
+# from the Garmin to a GPX archive folder (named with a UTC timestamp) and
+# imports the GPX tracks into driving_canonical.kml using a Python script.
+
+function Get-MTP-Object {
+  # Converts an MTP path string into a ComObject. Derived from:
+  # https://plusontech.com/2019/01/05/weekend-powershell-script-copy-files-from-phone-camera-by-month/
+  param(
+    $Shell,
+    [string]$PathString
+  )
+  $PathArray = $SourcePath -split "\\"
+  $MTPObject = $Shell.NameSpace(17).Self
+  foreach($item in $PathArray){
+    $MTPObject = $MTPObject.GetFolder.Items() | Where-Object{$_.Name -eq $item}
+  }
+  return $MTPObject
+}
 
 Set-Location -Path C:\Users\paulb\OneDrive\Projects\Maps\GPS\Auto
+$Shell = New-Object -ComObject Shell.Application
 $Time = Get-Date
 $UTCTime = $Time.ToUniversalTime().ToString("yyyyMMddTHHmmZ")
 
-# Back up Driving.kml.
-# Not needed, as update_kml.py handles the backup.
-# Write-Host "Backing up driving_canonical.kml ..."
-# Copy-Item ".\driving_canonical.kml" -Destination ".\.backup\driving_canonical.backup.kml"
-# Write-Host "...done."
-
 
 # Copy current.gpx to backup.
-# Copy from MTP device concept derived from:
-# https://plusontech.com/2019/01/05/weekend-powershell-script-copy-files-from-phone-camera-by-month/
-
 Write-Host "Copying Current.gpx from Garmin to GPX Archive ..."
-
-# $SourcePath = "Garmin DriveSmart 55\Internal Storage\GPX\Current.gpx"
-# $SourcePath = "Garmin DriveSmart 66\Internal Storage\GPX\Current.gpx"
-$SourcePath = "DriveSmart 50\Internal Storage\GPX\Current.gpx"
-$SourcePathArray = $SourcePath -split "\\"
-
-$Shell = New-Object -ComObject Shell.Application
-$CurrentGPX = $Shell.NameSpace(17).Self
-foreach($item in $SourcePathArray){
-  $CurrentGPX = $CurrentGPX.GetFolder.Items() | Where-Object{$_.Name -eq $item}
-}
+# $DeviceName = "Garmin DriveSmart 55"
+# $DeviceName = "Garmin DriveSmart 66"
+$DeviceName = "DriveSmart 50"
+$SourceGPXPath = $DeviceName + "\Internal Storage\GPX\Current.gpx"
+$SourceGPX = Get-MTP-Object $Shell $SourceGPXPath
 
 $TargetFolderShell = $Shell.NameSpace("C:\Users\paulb\OneDrive\Projects\Maps\GPS\Auto\raw\garmin").self
-$TargetFolderShell.GetFolder.CopyHere($CurrentGPX)
+$TargetFolderShell.GetFolder.CopyHere($SourceGPX)
 
 Rename-Item -Path ".\raw\garmin\Current.gpx" -NewName "$($UTCTime).gpx"
 Write-Host "...done."
