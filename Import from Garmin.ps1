afce7ffcd0af94aff962cf8a445a3fcb4000e74c
Paul Bogard <paulbogard@gmail.com>
2022-02-26 20:13:52 -0500

    Fix variable name in Get-MTP-Object

--- Import from Garmin.ps1
+++ Import from Garmin.ps1
@@ -9,7 +9,7 @@ function Get-MTP-Object {
     $Shell,
     [string]$PathString
   )
-  $PathArray = $SourcePath -split "\\"
+  $PathArray = $PathString -split "\\"
   $MTPObject = $Shell.NameSpace(17).Self
   foreach($item in $PathArray){
     $MTPObject = $MTPObject.GetFolder.Items() | Where-Object{$_.Name -eq $item}
@@ -24,18 +24,21 @@ $UTCTime = $Time.ToUniversalTime().ToString("yyyyMMddTHHmmZ")
 
 
 # Copy current.gpx to raw GPX data folder.
-Write-Host "Copying Current.gpx from Garmin to GPX Archive ..."
 # $DeviceName = "Garmin DriveSmart 55"
 # $DeviceName = "Garmin DriveSmart 66"
 $DeviceName = "DriveSmart 50"
 $SourceGPXPath = $DeviceName + "\Internal Storage\GPX\Current.gpx"
+Write-Host "Reading GPX file from $($SourceGPXPath) ..."
 $SourceGPX = Get-MTP-Object $Shell $SourceGPXPath
 
 $TargetFolderPath = "C:\Users\paulb\OneDrive\Projects\Maps\GPS\Auto\raw\garmin"
+Write-Host "Copying GPX to $($TargetFolderPath) ..."
 $TargetFolderShell = $Shell.NameSpace($TargetFolderPath).self
 $TargetFolderShell.GetFolder.CopyHere($SourceGPX)
 
-Rename-Item -Path "$($TargetFolderPath)\Current.gpx" -NewName "$($UTCTime).gpx"
+$RawFileName = "$($UTCTime).gpx"
+Write-Host "Renaming GPX to $($RawFileName) ..."
+Rename-Item -Path "$($TargetFolderPath)\Current.gpx" -NewName $RawFileName
 Write-Host "...done."
 
 
