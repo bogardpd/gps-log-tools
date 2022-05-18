cef2515bfd5e8bd0923064d4274e18b45994369a
Paul Bogard <paulbogard@gmail.com>
2022-05-17 20:26:32 -0400

    Filter tracks with ignore.json in GPXFile

--- GPXFile.py
+++ GPXFile.py
@@ -3,19 +3,24 @@ Represents an instance of a GPX file's data.
 """
 
 import gpxpy
+import json
 from datetime import timezone
+from dateutil.parser import isoparse
 
 class GPXFile:
 
     ISO_8601_BASIC = "%Y%m%dT%H%M%SZ"
     SUFFIX = "gpx"
+    IGNORE_FILE = "ignore.json"
 
     def __init__(self, input_gpx_file) -> None:
         with open(input_gpx_file, 'r') as f:
             self.gpx = gpxpy.parse(f)
+            print(self.gpx)
         self.start_time = self.__get_start_time()
         self.timestamp_filename = self.__get_timestamp_filename()
         self.creator = self.__get_creator()
+        self.__load_ignored()
         self.__process_tracks()
 
     def __repr__(self) -> str:
@@ -35,6 +40,15 @@ class GPXFile:
         else:
             return None
 
+    def __load_ignored(self):
+        try:
+            with open(Path(__file__).parent / GPXFile.IGNORE_FILE, 'r') as f:
+                ignored = json.load(f)
+            self.ignored_trk = [isoparse(dt) for dt in ignored['trk']]
+            self.ignored_trkseg = [isoparse(dt) for dt in ignored['trkseg']]
+        except FileNotFoundError:
+            return {'trk': [], 'trkseg': []}
+
     def __get_start_time(self):
         """Gets the UTC time of the first waypoint."""
         return min([
@@ -49,11 +63,24 @@ class GPXFile:
         return f"{filename}.{GPXFile.SUFFIX}"
 
     def __process_tracks(self):
+        """Creates a list of Tracks."""
         self.tracks = []
         for trk in self.gpx.tracks:
+            if GPXFile.trk_start(trk) in self.ignored_trk:
+                continue
             for trkseg in trk.segments:
+                if GPXFile.trkseg_start(trkseg) in self.ignored_trkseg:
+                    continue
                 self.tracks.append(Track(trkseg, self.creator))
 
+    def trk_start(trk):
+        """Gets the time of the first point of a GPX trk."""
+        return trk.segments[0].points[0].time.astimezone(timezone.utc)
+
+    def trkseg_start(trkseg):
+        """Gets the time of the first point of a GPX trkseg."""
+        return trkseg.points[0].time.astimezone(timezone.utc)
+        
 
 class Track:
     """A collection of GPS trackpoints forming a line."""
@@ -127,4 +154,6 @@ if __name__ == "__main__":
     # gf = GPXFile(sample['garmin'])
     gf = GPXFile(sample['mytracks'])
     pprint(gf)
-    pprint(gf.tracks[0].points[0:5])
+    # pprint(gf.tracks[0].points[0:5])
+    # pprint(gf.ignored_trk)
+    # pprint(gf.ignored_trkseg)
