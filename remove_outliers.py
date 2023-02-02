6b407e559fcb81a9ec370e6ea5bc80a27350a337
Paul Bogard <paulbogard@gmail.com>
2023-02-01 21:22:43 -0500

    Add teleportation outlier check to remove_outliers.py

--- remove_outliers.py
+++ remove_outliers.py
@@ -2,13 +2,19 @@ import argparse
 import gpxpy
 import colorama
 import csv
-from datetime import datetime
 from pathlib import Path
+from geopy.distance import distance
 
 colorama.init(autoreset=True)
 
 # Maximum consecutive points forward or back to look for time travel.
 MAX_TIME_TRAVEL_LEN = 60
+
+# Maximum consecutive points forward or back to look for teleportation.
+MAX_TELEPORT_LEN = 60
+
+# Maximum allowable speed of travel between two consecutive points.
+SPEED_THRESHOLD = 250.0 # meters per second
 
 def main(args):
     print(f"Removing outliers from {args.gpx_file}.")
@@ -75,7 +81,9 @@ def trkseg_remove_time_travel(segment, str_gpx_filename=None, log_csv=None):
                 print("Prior good point", times[bti-1])
                 time_travel_count = next(
                     j
-                    for j, t in enumerate(times[bti:bti+MAX_TIME_TRAVEL_LEN])
+                    for j, t in enumerate(
+                        times[bti:max(bti+MAX_TIME_TRAVEL_LEN,len(times))]
+                    )
                     if t > times[bti-1]
                 )
                 time_travel_indexes = list(range(bti,bti+time_travel_count))
@@ -132,7 +140,75 @@ def trkseg_remove_time_travel(segment, str_gpx_filename=None, log_csv=None):
 
 def trkseg_remove_teleportation(segment, str_gpx_filename=None, log_csv=None):
     """Removes position teleportation outliers from a GPX track segment."""
+    
+    # Generate list of points immediately preceeding a high speed.
+    points_before_speeding = list(
+        i
+        for i, (p1, p2)
+        in enumerate(zip(segment.points[:-1], segment.points[1:]))
+        if speed_m_s(p1, p2) >= SPEED_THRESHOLD
+    )
+
+    # Determine if each point is the last point of the correct location,
+    # or the last point of a teleported location. From this point, look
+    # forward and find out how many subsequent points exceed the speed
+    # threshold from it. If the count of those points is less than
+    # MAX_TELEPORT_LEN, it is likely a teleportation and should be
+    # removed.
+    outliers_teleportation = []
+    for i_pbs in points_before_speeding:
+        # Determine how many outlier points in a row are present.
+        try:
+            # Find next point that's not teleporting.
+            teleport_count = next(
+                j
+                for j in range(
+                    1, min(MAX_TELEPORT_LEN, len(segment.points) - i_pbs)
+                )
+                if speed_m_s(
+                    segment.points[i_pbs], segment.points[i_pbs+j]
+                ) < SPEED_THRESHOLD
+            ) - 1
+            teleport_indexes = list(range(i_pbs+1, i_pbs+teleport_count+1))
+            outliers_teleportation.extend(teleport_indexes)
+        except StopIteration:
+            pass
+
+    if len(outliers_teleportation) > 0:
+        # Document teleportation outliers.
+        for i in outliers_teleportation:
+            print(
+                colorama.Back.YELLOW
+                + colorama.Fore.BLACK
+                + f"Teleportation: {str_gpx_filename} at {segment.points[i]}"
+            )
+            if log_csv is not None:
+                with open(log_csv, 'a', newline='', encoding='utf-8') as lf:
+                    writer = csv.writer(lf)
+                    writer.writerow([
+                        str_gpx_filename,
+                        str(segment.points[i].time),
+                        str(segment.points[i].latitude),
+                        str(segment.points[i].longitude),
+                        "teleportation"
+                    ])
+
+        # Remove time travel outliers.
+        segment.points = list(
+            p for i, p in enumerate(segment.points)
+            if i not in outliers_teleportation
+        )
+    
     return segment
+
+def speed_m_s(p1, p2):
+    """Calculates the speed of travel between two GPX waypoints (m/s)."""
+    dist = distance(
+        (p1.latitude, p1.longitude), (p2.latitude, p2.longitude)
+    ).meters
+    timediff = (p2.time - p1.time).total_seconds()
+    return (dist / timediff)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
