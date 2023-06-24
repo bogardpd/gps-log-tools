b87274391e4b698bcfbeb90246f64de7e26d67ee
Paul Bogard <paulbogard@gmail.com>
2023-06-23 23:12:53 -0400

    Remove DrivingTrack.get_kml_placemark()

--- DrivingTrack.py
+++ DrivingTrack.py
@@ -9,12 +9,6 @@ import tomli
 with open(Path(__file__).parent / "config.toml", 'rb') as f:
     CONFIG = tomli.load(f)
 
-EXT_DATA_ATTRIBUTES = {
-    'creator':       "Creator",
-    'role':          "Role",
-    'vehicle_owner': "Vehicle Owner",
-}
-
 class DrivingTrack:
     """An instance of a driving log track."""
     def __init__(self, id_timestamp) -> None:
@@ -57,49 +51,3 @@ class DrivingTrack:
             'comments': self.description,
             'source_track_timestamp': self.timestamp,
         }
-
-    def get_kml_placemark(self):
-        """Returns a KML Placemark for the track."""
-        # Build name and description.
-        pm_name = self.timestamp.strftime(CONFIG['timestamps']['kml_name'])
-        if self.is_new:
-            pm_name += " (new)"
-        pm_desc = (
-            KML.description(self.description) if self.description
-            else None
-        )
-
-        # Build geometry.
-        coord_str = " ".join(
-            ",".join(
-                str(t) for t in coord[0:2] # Remove altitude if present
-            ) for coord in self.coords
-        )
-        
-        # Build ExtendedData.
-        ext_data_elements = [
-            KML.Data(
-                KML.displayName(display_name),
-                KML.value(getattr(self, attr_name)),
-                name=attr_name,
-            )
-            for attr_name, display_name in EXT_DATA_ATTRIBUTES.items()
-            if getattr(self, attr_name)
-        ]
-        
-        # Create Placemark.
-        pm = KML.Placemark(
-            KML.name(pm_name),
-            pm_desc,
-            KML.ExtendedData(*ext_data_elements),
-            KML.TimeStamp(
-                KML.when(self.timestamp.isoformat())
-            ),
-            KML.styleUrl("#1"),
-            KML.altitudeMode("clampToGround"),
-            KML.tessellate(1),
-            KML.LineString(
-                KML.coordinates(coord_str),
-            ),
-        )
-        return pm
