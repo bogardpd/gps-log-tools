7cc9099f7754c2a56ceaeddcb0ba5581f3f5fc8f
Paul Bogard <paulbogard@gmail.com>
2025-02-03 19:06:43 -0500

    Allow updates when one of pickup or return are not set

--- update_rental_times.py
+++ update_rental_times.py
@@ -2,6 +2,7 @@ import sqlite3
 import tomli
 from pathlib import Path
 from datetime import datetime
+from dateutil import parser
 from zoneinfo import ZoneInfo
 
 with open(Path(__file__).parent / "config.toml", 'rb') as f:
@@ -31,11 +32,13 @@ def update_rental_times():
                 pickup.location_name AS pickup_location,
                 return.location_name AS return_location,
                 pickup.discriminator AS pickup_discriminator,
-                return.discriminator AS return_discriminator
+                return.discriminator AS return_discriminator,
+                pickup_time_utc,
+                return_time_utc
             FROM rentals
-            JOIN rental_locations AS pickup
+            LEFT OUTER JOIN rental_locations AS pickup
                 ON rentals.pickup_rental_location_fid = pickup.fid
-            JOIN rental_locations AS return
+            LEFT OUTER JOIN rental_locations AS return
                 ON rentals.return_rental_location_fid = return.fid
             WHERE rentals.fid = ?
         """
@@ -44,58 +47,113 @@ def update_rental_times():
         if rental is None:
             print(f"Rental with feature ID {fid} not found.")
             return
-        pickup_date = datetime.strptime(rental[0], "%Y-%m-%d").date()
-        return_date = datetime.strptime(rental[1], "%Y-%m-%d").date()
+        pickup_date = (
+            datetime.strptime(rental[0], "%Y-%m-%d").date()
+            if rental[0] else None
+        )
+        return_date = (
+            datetime.strptime(rental[1], "%Y-%m-%d").date()
+            if rental[1] else None
+        )
         pickup_tz = rental[2]
         return_tz = rental[3]
         pickup_location = format_loc_name(rental[4], rental[6], rental[8])
         return_location = format_loc_name(rental[5], rental[7], rental[9])
+        pickup_new_time_utc = rental[10]
+        return_new_time_utc = rental[11]
         print(f"Pickup: {pickup_date} @ {pickup_location}")
         print(f"Return: {return_date} @ {return_location}")
-        pickup_time = input(
-            f"Enter the start time on {pickup_date} in {pickup_tz} (HH:MM): "
+        
+        pickup_new_time_utc, pickup_unchanged = request_time(
+            "pickup", pickup_date, pickup_tz, pickup_new_time_utc
         )
-        pickup_dt_utc = create_datetime(pickup_date, pickup_time, pickup_tz)
-        if pickup_dt_utc is None:
-            print("No pickup time")
-        else:
-            print(pickup_dt_utc.strftime(ISO_FORMAT))
-        return_time = input(
-            f"Enter the start time on {return_date} in {return_tz} (HH:MM): "
-        )
-        return_dt_utc = create_datetime(return_date, return_time, return_tz)
-        if return_dt_utc is None:
-            print("No return time")
-        else:
-            print(return_dt_utc.strftime(ISO_FORMAT))
+
+        return_new_time_utc, return_unchanged = request_time(
+            "return", return_date, return_tz, return_new_time_utc
+        )        
+
+        if pickup_unchanged and return_unchanged:
+            print("No changes made.")
+            return
+        
         proceed = input("Proceed with updating the rental times? (y/n): ")
         if proceed.lower() == 'y':
-            cur.execute(
-                """
-                    UPDATE rentals SET pickup_time_utc = ?, return_time_utc = ?
-                    WHERE fid = ?
-                """,
-                (pickup_dt_utc, return_dt_utc, fid)
-            )
+            if (not pickup_unchanged) and return_unchanged:
+                # Only pickup time changed
+                cur.execute(
+                    "UPDATE rentals set pickup_time_utc = ? WHERE fid = ?",
+                    (pickup_new_time_utc, fid),
+                )
+            elif pickup_unchanged and (not return_unchanged):
+                # Only return time changed
+                cur.execute(
+                    "UPDATE rentals set return_time_utc = ? WHERE fid = ?",
+                    (return_new_time_utc, fid),
+                )
+            else:
+                # Both times changed
+                cur.execute(
+                    """
+                        UPDATE rentals
+                        SET pickup_time_utc = ?, return_time_utc = ?
+                        WHERE fid = ?
+                    """,
+                    (pickup_new_time_utc, return_new_time_utc, fid)
+                )
             conn.commit()
             print(f"Updated rental with feature ID {fid}.")
         else:
             print("Aborted.")
-
-def create_datetime(date, time, tz):
-    if len(time) == 0:
-        return None
-    hour, minute = [int(t) for t in time.split(":")]
-    return datetime(
-        date.year, date.month, date.day,
-        hour, minute, tzinfo=ZoneInfo(tz)
-    ).astimezone(ZoneInfo("UTC"))
 
 def format_loc_name(agency, location, discriminator):
     output = f"{agency} {location}"
     if discriminator:
         output += f" [{discriminator}]"
     return output
+
+def request_time(type, date, tz, time_utc_str):
+    """Request a time from the user and return a datetime object."""
+    def input_time(type, date, tz):
+        time = input(
+            f"Enter the {type} time on {date} in {tz} (HH:MM): "
+        )
+        if len(time) == 0:
+            return None
+        hour, minute = [int(t) for t in time.split(":")]
+        return datetime(
+            date.year, date.month, date.day,
+            hour, minute, tzinfo=ZoneInfo(tz)
+        ).astimezone(ZoneInfo("UTC"))
+    
+    if time_utc_str is None:
+        cur_time_utc = None
+    else:
+        cur_time_utc = parser.isoparse(time_utc_str)
+
+    if tz is None:
+        print(f"No {type} time zone, skipping.")
+        new_time_utc = None
+        unchanged = True
+    else:
+        if cur_time_utc is not None:
+            print(f"Current {type} time: {cur_time_utc.strftime(ISO_FORMAT)}")
+            update = input(f"Change {type} time? (y/N): ")
+            if update.lower() == 'y':
+                new_time_utc = input_time(type, date, tz)
+            else:
+                new_time_utc = cur_time_utc
+        else:
+            new_time_utc = input_time(type, date, tz)
+        if new_time_utc is None:
+            print(f"No {type} time", end="")
+        else:
+            print(new_time_utc.strftime(ISO_FORMAT), end="")
+        unchanged = (cur_time_utc == new_time_utc)
+        if unchanged:
+            print(" (Unchanged)")
+        else:
+            print() # newline
+    return (new_time_utc, unchanged)
 
 
 if __name__ == '__main__':
